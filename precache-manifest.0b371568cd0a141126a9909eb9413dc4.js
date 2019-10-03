82a44dc89d7560295e20823a1489ebb034c1a2dc
Francis Brito <fr.br94@gmail.com>
2019-10-02 22:44:59 -0400

    Updates

---
+++ precache-manifest.0b371568cd0a141126a9909eb9413dc4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d9fd296e52ae4449a9f9299310a263",
+    "url": "/rnd2019-would-you-rather/index.html"
+  },
+  {
+    "revision": "1a2ddfed7233ab1a8b40",
+    "url": "/rnd2019-would-you-rather/static/css/main.3cce7fb5.chunk.css"
+  },
+  {
+    "revision": "ab712c0c4fcb8c507b94",
+    "url": "/rnd2019-would-you-rather/static/js/2.0a420d4a.chunk.js"
+  },
+  {
+    "revision": "1a2ddfed7233ab1a8b40",
+    "url": "/rnd2019-would-you-rather/static/js/main.90e26d33.chunk.js"
+  },
+  {
+    "revision": "af8a5d267209e67d5d51",
+    "url": "/rnd2019-would-you-rather/static/js/runtime~main.a50d03e5.js"
+  },
+  {
+    "revision": "461c9f248ac06f1a148742f2bf2b3819",
+    "url": "/rnd2019-would-you-rather/static/media/add.461c9f24.svg"
+  },
+  {
+    "revision": "71070825719c9f8ceac6e5ab31557f16",
+    "url": "/rnd2019-would-you-rather/static/media/check.71070825.svg"
+  },
+  {
+    "revision": "bf513257b286c67b2534c894306df100",
+    "url": "/rnd2019-would-you-rather/static/media/circle-check.bf513257.svg"
+  },
+  {
+    "revision": "dfa639536dee745f989e7768de89dfad",
+    "url": "/rnd2019-would-you-rather/static/media/down-arrow.dfa63953.svg"
+  },
+  {
+    "revision": "4fcf3fc659b8dbfd682daf14ae0dd739",
+    "url": "/rnd2019-would-you-rather/static/media/help.4fcf3fc6.svg"
+  },
+  {
+    "revision": "cbd8b5a8a02328308f76296212fe3d72",
+    "url": "/rnd2019-would-you-rather/static/media/polls.cbd8b5a8.svg"
+  },
+  {
+    "revision": "91da60531102e1686f6016e61a973a03",
+    "url": "/rnd2019-would-you-rather/static/media/scoreboard.91da6053.svg"
+  }
+]);
